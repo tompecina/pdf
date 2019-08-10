90ba74d9f2cf889e4e8973850549aa5607aa595f
Tomáš Pecina <tomas@pecina.cz>
2019-08-10 14:29:35 +0200

    Source code source added to notice

--- src/cz/pecina/pdf/stamppdf/Parameters.java
+++ src/cz/pecina/pdf/stamppdf/Parameters.java
@@ -16,6 +16,8 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The source code is available from <https://github.com/tompecina/pdf>.
  */
 
 package cz.pecina.pdf.stamppdf;
